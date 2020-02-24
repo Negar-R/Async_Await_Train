4d6a32cad3d485fbcb187b0993911f4c599a6b54
Negar-R <negar.r.78.ioi@gmail.com>
2020-02-24 21:57:32 +0330

    an async program that fetches json from Divar

---
+++ DivarApi.py
@@ -0,0 +1,35 @@
+import aiohttp
+import asyncio
+import json
+
+
+async def get_json(client , url):
+    async with client.get(url) as response:
+        assert response.status == 200
+        return await response.read()
+
+
+async def get_divar_top(client , subreddit):
+    content = await get_json(client , 'https://api.divar.ir/v8/web-search/tehran' + subreddit)
+    data = json.loads(content.decode('utf-8'))
+    print('** ** ' , type(data))
+    for i in data['widget_list']:
+        title = i['data']['title']
+        description = i['data']['description']
+        image_url = i['data']['image']
+
+        print(title + ' : ' + description + ' ( ' + image_url + ' )')
+
+    print('DONE:', subreddit + '\n')
+
+
+async def main(client):
+    await asyncio.gather(get_divar_top(client , '/car?q='+'پراید') ,
+                        get_divar_top(client , '/car?q='+'206') ,
+                        get_divar_top(client , '/car?q='+'پیکان'))
+
+loop = asyncio.get_event_loop()
+client = aiohttp.ClientSession(loop = loop) # Client session is the recommended interface for making HTTP requests.
+
+asyncio.ensure_future(main(client))
+loop.run_forever()
